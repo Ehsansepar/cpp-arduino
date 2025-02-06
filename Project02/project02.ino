eaafb8ed713d7baf2614d675a5092f5128fd8ea4
Ehsansepar <ehsan.separ007@gmail.com>
2025-02-06 21:36:03 +0100

    Add SOS mode with button hold functionality

---
+++ Project02/project02.ino
@@ -0,0 +1,73 @@
+const int ledRouge = 2;  // LED rouge
+const int ledOrange = 3; // LED orange
+const int ledVert = 4;   // LED vert
+const int boutonSOS = 5; // Bouton SOS
+
+bool sosActif = false;
+unsigned long dernierClignotement = 0;
+bool etatClignotement = false;
+unsigned long debutAppui = 0;
+int dernierEtat = 0;
+
+void setup()
+{
+    pinMode(ledRouge, OUTPUT);
+    pinMode(ledOrange, OUTPUT);
+    pinMode(ledVert, OUTPUT);
+    pinMode(boutonSOS, INPUT_PULLUP);
+    Serial.begin(9600);
+
+    digitalWrite(ledRouge, LOW);
+    digitalWrite(ledOrange, LOW);
+    digitalWrite(ledVert, LOW);
+}
+
+void loop()
+{
+    // Lecture du bouton
+    bool boutonAppuye = (digitalRead(boutonSOS) == LOW);
+
+    // Premier appui sur le bouton - active le SOS
+    if (boutonAppuye && !sosActif && debutAppui == 0)
+    {
+        sosActif = true;
+        debutAppui = millis();
+        // Sauvegarder l'état
+        if (digitalRead(ledRouge) == HIGH)
+            dernierEtat = 0;
+        if (digitalRead(ledOrange) == HIGH)
+            dernierEtat = 1;
+        if (digitalRead(ledVert) == HIGH)
+            dernierEtat = 2;
+    }
+
+    // Si le bouton est maintenu appuyé et que le mode SOS est actif
+    if (boutonAppuye && sosActif)
+    {
+        if ((millis() - debutAppui) >= 5000)
+        {
+            // Désactiver le mode SOS
+            sosActif = false;
+
+            // Restaurer l'état précédent - version simple
+            digitalWrite(ledRouge, LOW);
+            digitalWrite(ledOrange, LOW);
+            digitalWrite(ledVert, LOW);
+
+            if (dernierEtat == 0)
+            {
+                digitalWrite(ledRouge, HIGH);
+            }
+            if (dernierEtat == 1)
+            {
+                digitalWrite(ledOrange, HIGH);
+            }
+            if (dernierEtat == 2)
+            {
+                digitalWrite(ledVert, HIGH);
+            }
+        }
+    }
+
+    
+}
